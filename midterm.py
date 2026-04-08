076297aa719e6ab9f98cc2f412052074c5c886e4
Jeremy Nally <jnally3@ivytech.edu>
2026-04-08 16:13:19 +0000

    Write text transmission simulator portion

--- midterm.py
+++ midterm.py
@@ -1,4 +1,5 @@
 import hashlib
+from cryptography.fernet import Fernet
 
 # take string and generate SHA-256 hash
 def make_text_hash(text):
@@ -28,6 +29,28 @@ def make_file_hash(filepath, block_size=65536):
         return 'Error: File not found.'
     except Exception as e:
         return 'Error: ' + e
+    
+# generate AES key using fernet from cryptography
+def generate_key():
+    return Fernet.generate_key()
+    
+# AES encryption using fernet from cryptography
+def text_encrypt(text, key):
+    cipher = Fernet(key)
+    return cipher.encrypt(text.encode('utf-8'))
+
+# AES decryption using fernet from cryptography
+def text_decrypt(text, key):
+    cipher = Fernet(key)
+    return cipher.decrypt(text.encode('utf-8'))
+
+# TODO make AES file encryption happen
+def file_encrypt():
+    return
+
+# TODO make AES file decryption happen
+def file_decrypt():
+    return
 
 # present menu for SHA-256 hashing app
 # option to hash text or hash a file using SHA-256 
@@ -40,22 +63,44 @@ def main():
     # loop until 1 or 2 is not selected
     while keep_going:
         # menu
-        print('\nModule 3: Assignment - Secure Hashing and Encryption')
-        print('SHA-256 Hashing App')
+        print('\nModule 4 Midterm: Build a Secure Data Transmission App with Hashing and Encryption')
+        print('Secure Data Transmission App Simulator (with SHA-256 Hashing and AES Encryption)')
         print('\nOptions:')
-        print('1. Hash a text string')
-        print('2. Hash a file')
+        print('1. Input text to transmit')
+        print('2. Input file to transmit')
         print('3. Exit')
         
         # get menu selection
         choice = input('\nSelect an option (1, 2, or 3): ')
         
-        # hash text
+        # secure text transmission
         if choice == '1':
-            text_input = input('\nEnter the text to hash: ')
-            print('\nSHA-256 Hash:', make_text_hash(text_input),'\n')
+            text_input = input('\nEnter the text to transmit: ')
+
+            text_input_hash = make_text_hash(text_input)
+            print('\nOriginal Text SHA-256 Hash:', text_input_hash)
             
-        # hash file
+            key = generate_key()
+            print('\nGenerated AES Shared Key (must be communicated secretly ahead of time):', key.decode('utf-8'))
+            
+            ciphertext = text_encrypt(text_input, key).decode('utf-8')
+            print('\nEncrypted Text:', ciphertext)
+            
+            print('\nPretend encrypted message and hash was sent to someone with the shared key...')
+            
+            decrypted_text = text_decrypt(ciphertext, key).decode('utf-8')
+            print('\nDecrypted Text:', decrypted_text)
+
+            decrypted_text_hash = make_text_hash(decrypted_text)
+            print('\nDecrypted Text SHA-256 Hash:', decrypted_text_hash)
+            
+            print('Check if original text hash (', text_input_hash, ') matches decrypted text hash (', decrypted_text_hash, ')...\n')
+            if text_input_hash == decrypted_text_hash:
+                print('The hashes match! Message integrity verified.')
+            else:
+                print('The hashes do not match. Message integrity not verified.')
+            
+        # secure file transmission
         elif choice == '2':
             # Note: The file needs to be in the same directory as the script, 
             # or you must provide the absolute path.
